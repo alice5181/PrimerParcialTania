e731134031afdb293c38dba94d1e0bc12b5d8e24
TANIA ALICIA RODRIGUEZ <alice5181.trg@gmil.com>
2023-08-16 22:08:00 -0300

    comitendo app y card

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,70 @@
+import React, { useState } from 'react';
 import './App.css'
 import Card from './components/Card'
 
 function App() {
+    const [nombreLibro, setNombreLibro] = useState('');
+    const [autorLibro, setAutorLibro] = useState('');
+    const [nombreEstudiante, setNombreEstudiante] = useState('');
+    
+
+    const [showCard, setShowCard] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
   
+        // Validaciones
+        if ((nombreLibro.trim().length < 3 || /^\s+/.test(nombreLibro)) || (autorLibro.length < 6) || (nombreEstudiante.length < 6))  {
+          setErrorMessage('Por favor chequea que la información proporcionada sea correcta');
+          setShowCard(false);
+          return;
+        }
+        // Si pasa validaciones
+        setShowCard(true);
+        setErrorMessage('');
+    };
 
   return (
-    <div className='App'>
+    <div>
       
-      <h1>Carga de estudiantes</h1>
-      <form></form>
+      <h1>Solicitud de préstamos en biblioteca</h1>
 
-      <Card />
+      <form onSubmit={handleSubmit} className="main-form">
+        <label>Título del libro solicitado: </label>
+        <input
+          type="text"
+          name="nombreLibro"
+          value={nombreLibro}
+          onChange={(event) => setNombreLibro(event.target.value)}
+        />
+
+        <label>Nombre completo del autor del libro: </label>
+        <input
+          type="text"
+          name="autorLibro"
+          value={autorLibro}
+          onChange={(event) => setAutorLibro(event.target.value)}
+        />
+
+        <label>Nombre completo del estudiante que solicita el préstamo </label>
+          <input
+            type="text"
+            name="nombreEstudiante"
+            value={nombreEstudiante}
+            onChange={(event) => setNombreEstudiante(event.target.value)}
+          />
+          
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+        <button>Solicitar Préstamo</button>
+      </form>
+
+      {showCard && <Card Libro={nombreLibro} Estudiante={nombreEstudiante} Autor={autorLibro} />}
     </div>
-  )
-}
+  );
+
+  }
+
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 import './Card.css'
 
 
-const Card = () => {
-
-
-    return <div>Hola, soy una tarjeta</div>;
+const Card = ({Libro, Autor, Estudiante}) => {
+    return <div className="card">
+        <h2>Aprobación de préstamo para:</h2>
+            <p>Libro: {Libro} , Autor: {Autor} </p>
+            <p>Estudiante: {Estudiante}</p>
+        </div>;
 };
 
 
